f7e820097ed27079ef0998d0dac31cd47d0fd0eb
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-29 15:46:22 +0530

    Prediction Pipeline

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # End_To_End_DS-Project
 
 
-import dagshub
-dagshub.init(repo_owner='Rizwansaifi571', repo_name='End_To_End_DS-Project', mlflow=True)
-
-import mlflow
-with mlflow.start_run():
-  mlflow.log_param('parameter name', 'value')
-  mlflow.log_metric('metric name', 1)
--- app.py
+++ app.py
@@ -2,30 +2,45 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
-from mlproject.logger import logging
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
 from mlproject.exception import CustomException
-from mlproject.components.data_ingestion import DataIngestion
-from mlproject.components.data_ingestion import DataIngestionConfig
-from mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
-from mlproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
+from sklearn.preprocessing import StandardScaler
+from mlproject.pipelines.prediction_pipeline import CustomData, PredictPipeline
 
-if __name__=="__main__":
-    logging.info("The execution has started")
+application = Flask(__name__)
 
-    try:
-        #data_ingestion_config=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+app = application
 
-        #data_transformation_config=DataTransformationConfig()
-        data_transformation=DataTransformation()
-        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+# Route for a home page
 
-        ## Model Training
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        model_trainer=ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-        
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys)
+@app.route('/predictdata', methods = ['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+
+        predict_Pipeline = PredictPipeline()
+        results = predict_Pipeline.predict(pred_df)
+        return render_template('home.html', results = results[0])
+    
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True)
